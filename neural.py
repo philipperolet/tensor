0fa8354db67a3723e038033802879e381d98551c
sittingbull <pr@little-big-data.com>
2017-09-20 20:32:17 +0200

    Previous commit

--- neural.py
+++ neural.py
@@ -2,16 +2,19 @@ import numpy as np
 
 
 class NeuralNet(object):
-    """
-    """
 
     def __init__(self, nb_neurons=8, input_size=3):
         self.nb_neurons = nb_neurons
-        self.input_size = 3
-        self.weights = np.zeros((nb_neurons, input_size))
+        self.input_size = input_size
+        self._weights = np.zeros((input_size, nb_neurons))
+        self._output_weights = np.zeros(nb_neurons)
 
     def run(self, x):
+        """Runs the network on an input array"""
         if (not(isinstance(x, list)) or len(x) != self.input_size):
             raise TypeError("Input should be list of size {}".format(self.input_size))
-        return 0.5
+        return np.dot(sigmoid(np.dot(np.array(x), self._weights)), self._output_weights)
 
+
+def sigmoid(x):
+    return 1/(1+np.exp(np.negative(x)))
--- test_neural.py
+++ test_neural.py
@@ -19,11 +19,23 @@ class NeuralNetTest(unittest.TestCase):
         with self.assertRaises(TypeError):
             self.nn.run([4, 4, 3, 5])
 
-        # with weights at 0, two different inputs should yield 0.5
-        self.assertEqual(0.5, self.nn.run([1, 1, 1]))
-        self.assertEqual(0.5, self.nn.run([2, 2, 0]))
-
-        # with output weights at (0.5, 0.5, 1), should yield 0
-        self.nn._output_weights = [0.5, 0.5, 1.0]
+        # with weights at 0, two different inputs should yield 0
         self.assertEqual(0, self.nn.run([1, 1, 1]))
         self.assertEqual(0, self.nn.run([2, 2, 0]))
+
+        # with output weights summing to 10, should yield 5
+        self.nn._output_weights = [0.5, 0.5, 0.5, 0.5, 3, 3, 2, 0]
+        self.assertEqual(5, self.nn.run([1, 1, 1]))
+        self.assertEqual(5, self.nn.run([2, 2, 0]))
+
+        # a few tests with real weights
+        self.nn._output_weights = [1]*8
+        self.nn._weights = [
+            [1, -2, 2, 0, 0, 0, 0, 0],
+            [0, -1, -2, 0, 0, 0, 0, 0],
+            [1, -2, 0, 0, 0, 0, 0, 0]
+        ]
+
+        self.assertEqual(4.5, self.nn.run([2**50, 0, 2**50]))  # Large numbers for easy mental check
+        self.assertEqual(4.0, self.nn.run([2**51, 2**51, -2**50]))
+
