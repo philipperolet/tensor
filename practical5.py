0bf599f924e390067bd0d055f5c5accadf71d014
sittingbull <pr@little-big-data.com>
2019-03-11 12:34:56 +0100

    Next xp batch

--- practical5.py
+++ practical5.py
@@ -13,12 +13,15 @@ def generate_disc_set(nb):
     return tr_input, tr_target.type(torch.long).view(-1, 1)
 
 
-def generate_learning_data():
+def generate_learning_data(normalize=True, training_normalization=True):
     train_data, train_target = generate_disc_set(1000)
-    train_data.sub_(train_data.mean(dim=0)).div_(train_data.std(dim=0))
-
+    mean, std = train_data.mean(dim=0), train_data.std(dim=0)
     test_data, test_target = generate_disc_set(1000)
-    test_data.sub_(test_data.mean(dim=0)).div_(test_data.std(dim=0))
+    if normalize:
+        train_data.sub_(mean).div_(std)
+        if not(training_normalization):
+            mean, std = test_data.mean(dim=0), test_data.std(dim=0)
+        test_data.sub_(mean).div_(std)
 
     return dict(
         training_input=train_data,
@@ -28,7 +31,14 @@ def generate_learning_data():
     )
 
 
-def create_shallow_model():
+def create_shallow_model(last_relu=False):
+    if last_relu:
+        return nn.Sequential(
+            nn.Linear(2, 128),
+            nn.ReLU(),
+            nn.Linear(128, 2),
+            nn.ReLU(),
+        )
     return nn.Sequential(
         nn.Linear(2, 128),
         nn.ReLU(),
@@ -36,27 +46,32 @@ def create_shallow_model():
     )
 
 
-def create_deep_model():
+def create_deep_model(last_relu=False):
     modules_dict = OrderedDict()
     previous_size = 2
     for layer_size in [4, 8, 16, 32, 64, 2]:
         modules_dict["fc_{}".format(layer_size)] = nn.Linear(previous_size, layer_size)
         modules_dict["relu_{}".format(layer_size)] = nn.ReLU()
         previous_size = layer_size
-    del modules_dict["relu_2"]
+    if not(last_relu):
+        del modules_dict["relu_2"]
     return nn.Sequential(modules_dict)
 
 
 if __name__ == '__main__':
-    data = generate_learning_data()
     parameters = dict(
         steps=250,
         eta=0.1,
         minibatch_size=100
     )
 
-    def initialization_experiment(model_function, init_std=None):
-        model = model_function()
+    def initialization_experiment(model_function,
+                                  last_relu,
+                                  normalize,
+                                  training_normalization,
+                                  init_std=None):
+        data = generate_learning_data(normalize, training_normalization)
+        model = model_function(last_relu)
         if init_std is not None:
             with torch.no_grad():
                 for param in model.parameters():
@@ -65,11 +80,17 @@ if __name__ == '__main__':
         return Trainer(model, data, parameters, nn.CrossEntropyLoss()).train()
 
     def display(result):
-        print(
-            "Function {param_combination[model_function]}, initialization_param {param_combination[init_std]} : average {avg:.2%} ons {iterations} xps (std {std:.2%})".format(**result)
-        )
+        print((
+            "Function {param_combination[model_function].__name__}, "
+            "normalization {param_combination[normalize]}, "
+            "training_normalization {param_combination[training_normalization]}, "
+            "last_relu {param_combination[last_relu]} : "
+            "average {avg:.2%} on {iterations} xps (std {std:.2%})"
+        ).format(**result))
 
     results = Experimenter(initialization_experiment, display).experiment(
         {'model_function': [create_shallow_model, create_deep_model],
-         'init_std': [None, 0.001, 0.01, 0.1, 1, 10]},
+         'last_relu': [True, False],
+         'normalize': [True, False],
+         'training_normalization': [True, False]},
         iterations=10)
