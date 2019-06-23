3072de0d1f986b213be6f9d2fbbb4dbc9e95b973
sittingbull <philippe.rolet@artefact.com>
2019-06-23 17:50:58 +0200

    Added plotting for loss experiment 1

--- plot.py
+++ plot.py
@@ -1,7 +1,9 @@
-from matplotlib import pyplot as plt
 import json
-from itertools import product
+import numpy as np
 import sys
+
+from matplotlib import pyplot as plt
+from itertools import product
 
 
 def plot_with_error_bars(filename):
@@ -40,5 +42,44 @@ def plot_with_error_bars(filename):
     plt.show()
 
 
+def plot_new_loss_xp(filename):
+
+    with open(filename, 'r') as data_file:
+        data = json.load(data_file)['results']
+
+    for i, (dataset, ds_size) in enumerate(product(['mnist', 'cifar'], ['normal', 'full'])):
+        plt.figure(i)
+
+        # get data pertaining to this set & size
+        data_slice = filter(
+            lambda d: (d['param_combination']['dataset'] == dataset
+                       and d['param_combination']['dataset_size'] == ds_size),
+            data
+        )
+
+        # sort it according to exp_factor
+        data_slice = sorted(
+            data_slice,
+            key=lambda d: d['param_combination']['exp_factor']
+        )
+
+        # plot it
+        for i, d in product(range(len(data_slice) - 1), range(2)):
+            plt.title(f"{dataset} - {ds_size}")
+            plt.subplot(221+i)
+            averages = np.mean(data_slice[i + d]['values'], 0)
+            stds = np.std(data_slice[i + d]['values'], 0)
+
+            plt.errorbar(
+                range(len(averages)),
+                averages,
+                yerr=[2 * x for x in stds],
+                label="exp" + str(data_slice[i + d]['param_combination']['exp_factor']),
+                capsize=5,
+            )
+            plt.legend()
+    plt.show()
+
+
 if __name__ == "__main__":
-    plot_with_error_bars(sys.argv[1])
+    plot_new_loss_xp(sys.argv[1])
